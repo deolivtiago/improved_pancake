33e4433a756c14fbe57d112fcc1edebbff60cb73
Tiago Oliveira <olvrti@gmail.com>
2020-12-16 13:31:18 -0300

    feat: add validator to LoginScreen form

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,33 @@ class App extends StatelessWidget {
   }
 }
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  var _emailCtrl = TextEditingController();
+  var _passCtrl = TextEditingController();
+
+  bool _isValid() {
+    if (_formKey.currentState.validate()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  String _requiredFields(value) {
+    if (value.isEmpty) {
+      return 'Campo Obrigatório';
+    } else {
+      return null;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,47 +63,61 @@ class LoginScreen extends StatelessWidget {
             SizedBox(
               height: 20,
             ),
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                labelText: "E-Mail",
-                labelStyle: TextStyle(
-                  color: Colors.black87,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 20,
-                ),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: _emailCtrl,
+                    validator: _requiredFields,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: "E-Mail",
+                      labelStyle: TextStyle(
+                        color: Colors.black87,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    controller: _passCtrl,
+                    validator: _requiredFields,
+                    keyboardType: TextInputType.text,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: "Senha",
+                      labelStyle: TextStyle(
+                        color: Colors.black87,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: FlatButton(
+                      child: Text(
+                        "ENTRAR",
+                        style: TextStyle(
+                          color: Colors.black87,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 20,
+                        ),
+                      ),
+                      color: Theme.of(context).accentColor,
+                      padding: EdgeInsets.all(14),
+                      onPressed: _isValid,
+                    ),
+                  ),
+                ],
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              keyboardType: TextInputType.text,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: "Senha",
-                labelStyle: TextStyle(
-                  color: Colors.black87,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            FlatButton(
-              child: Text(
-                "ENTRAR",
-                style: TextStyle(
-                  color: Colors.black87,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 20,
-                ),
-              ),
-              color: Theme.of(context).accentColor,
-              padding: EdgeInsets.all(14),
-              onPressed: () {},
             ),
           ],
         ),
